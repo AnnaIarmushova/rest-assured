eb641275dfb0fcd55a99a4da43a1c708f26e968d
almalmalm <almostkalinin@gmail.com>
2023-09-16 14:32:32 +0300

    add api_tests using specs

--- src/test/java/ReqResTest.java
+++ src/test/java/ReqResTest.java
@@ -2,7 +2,10 @@ import io.restassured.http.ContentType;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
+import java.beans.Transient;
+import java.time.Clock;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import static io.restassured.RestAssured.given;
 
@@ -11,10 +14,11 @@ public class ReqResTest {
     @Test
     public void checkAvatarAndIdTest() {
 
+        Specifications.installSpecification(Specifications.requestSpec(URL),Specifications.responseSpecOk200());
+
         List<UserData> users = given()
                 .when()
-                .contentType(ContentType.JSON)
-                .get(URL + "api/users?page=2")
+                .get("api/users?page=2")
                 .then().log().all()
                 .extract().body().jsonPath()
                 .getList("data",UserData.class);
@@ -23,4 +27,80 @@ public class ReqResTest {
         Assert.assertTrue(users.stream().allMatch(x->x.getEmail().endsWith("@reqres.in")));
     }
 
+    @Test
+    public void successRegTest() {
+        Specifications.installSpecification(Specifications.requestSpec(URL),Specifications.responseSpecOk200());
+
+        Integer id = 4;
+        String token = "QpwL5tke4Pnpja7X4";
+
+        Register user = new Register("eve.holt@reqres.in", "pistol");
+
+        SuccessReg successReg = given()
+                .body(user)
+                .when()
+                .post("api/register")
+                .then().log().all()
+                .extract().as(SuccessReg.class);
+
+        Assert.assertNotNull(successReg.getId());
+        Assert.assertNotNull(successReg.getToken());
+
+        Assert.assertEquals(id,successReg.getId());
+        Assert.assertEquals(token,successReg.getToken());
+    }
+
+    @Test
+    public void unSuccessRegTest() {
+        Specifications.installSpecification(Specifications.requestSpec(URL),Specifications.responseSpecError400());
+
+        Register user = new Register("sydney@fife", "");
+        UnSuccessReg unSuccessReg = given()
+                .body(user)
+                .post("api/register")
+                .then().log().all()
+                .extract().as(UnSuccessReg.class);
+        Assert.assertEquals("Missing password", unSuccessReg.getError());
+    }
+
+    @Test
+    public void sortedYearsTest(){
+        Specifications.installSpecification(Specifications.requestSpec(URL),Specifications.responseSpecOk200());
+        List<ColorsData> colors = given()
+                .when()
+                .get("api/unknown")
+                .then().log().all()
+                .extract().body().jsonPath().getList("data",ColorsData.class);
+        List<Integer> years = colors.stream().map(ColorsData::getYear).collect(Collectors.toList());
+        List<Integer> sortedYears = years.stream().sorted().collect(Collectors.toList());
+        Assert.assertEquals(sortedYears,years);
+        System.out.println(years);
+        System.out.println(sortedYears);
+    }
+
+    @Test
+    public void deleteUserTest(){
+        Specifications.installSpecification(Specifications.requestSpec(URL),Specifications.responseSpecUnique(204));
+        given()
+                .when()
+                .delete("api/users/2")
+                .then().log().all();
+    }
+
+@Test
+    public void timeTest() {
+    Specifications.installSpecification(Specifications.requestSpec(URL),Specifications.responseSpecOk200());
+    UserTime user = new UserTime("morpheus","zion resident");
+    UserTimeResponse response = given()
+            .body(user)
+            .when()
+            .put("api/users/2")
+            .then().log().all()
+            .extract().as(UserTimeResponse.class);
+    //String regex = "(.{5})$";
+    String currentTime = Clock.systemUTC().instant().toString();
+    //Assert.assertEquals(currentTime,response.getUpdatedAt().replaceAll(regex,""));
+    System.out.println(currentTime);
+
+}
 }
---
+++ src/test/java/UnSuccessReg.java
@@ -0,0 +1,14 @@
+public class UnSuccessReg {
+    private String error;
+
+    public UnSuccessReg(String error) {
+        this.error = error;
+    }
+
+    public UnSuccessReg() {
+    }
+
+    public String getError() {
+        return error;
+    }
+}
